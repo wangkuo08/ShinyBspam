3be7f910b867f55552b5a95ee2ab9c1810c581f8
Kuo <wangp@smu.edu>
2023-03-22 23:31:38 -0500

    add welcome tab

--- ui.R
+++ ui.R
@@ -6,8 +6,49 @@ library(bspam)
 ui <- fluidPage(
   navbarPage("BSpam Shiny", theme = shinytheme("lumen"),
              # add welcome tab
-             #
-             tabPanel("Select Dataset", fluid = TRUE, icon = icon("globe-americas"),
+             navbarMenu("Welcome", icon = icon("info-circle"),
+                        tabPanel("Welcome Page", fluid = TRUE,
+                                 fluidRow(
+                                   column(6,
+                                          h4(p("Shiny bspam")),
+                                          h5(p("US News and World Report uses four categories of schools for their rankings system:"),
+                                             p("National universities are those that offer a “full range” of undergraduate majors, while also offering graduate programs, including at the doctoral level.  Intercollegiate sports, including swimming, are generally pursued by undergrads, or occasionally students in master’s degree programs, so a university having nor not having doctoral programs isn’t directly relevant.  That said, doctoral programs and faculty research go hand-in-hand, so faculty at national universities are nearly always active in research, in addition to their teaching duties.  National universities are usually, though not always, large.  Most state flagship universities would fall under this category."),
+                                             p("Regional universities are similar to national universities in that they have a wide range of undergrad programs, and some master’s programs as well.  They generally do not have large doctoral programs, and correspondingly less faculty research."),
+                                             p("National liberal arts colleges are undergraduate focused, with few graduate programs.  They award the majority of their degrees in arts and sciences, and may or may not have other undergraduate programs, like engineering or professional studies."),
+                                             p("Regional colleges are also undergraduate focused institutions, but do not award the majority of their degrees in arts and/or sciences.  These colleges may have a particular focus, like technology or agriculture, or they may be primarily two year institutions that also grant some four year degrees.")
+                                          )
+                                   ),
+                                   column(6,
+                                          h4(p("US News Rankings")),
+                                          h5(p("Every year the US News and World Report issues a set of rankings for US colleges and universities.  They are a used in this setting as a guideline, and a general comparative device, but can often be misinterpreted or overvalued.  The major component of a given school’s rankings are graduation and retention rates, academic reputation (basically name recognition), and faculty resources (class size, faculty salary etc.).  Each school is given a score, and then placed in order.  That said the scored differences between schools of different rank can be quite small, so take the rankings with a grain of salt.
+                                    The full methodology for the US News and World report college rankings can be found ",
+                                               a("here.",
+                                                 href = "https://www.usnews.com/education/best-colleges/articles/ranking-criteria-and-weights"))
+                                          )
+                                   ))
+
+                        ),
+
+                        tabPanel("About", fluid = TRUE,
+                                 fluidRow(
+                                   column(6,
+                                          #br(),
+                                          h4(p("About the Project")),
+                                          h5(p("This project is intended to facilitate useful comparisons between colleges in the NCAA, based on swimming performance, location, and academic information.  Here a prospective student-athlete, or anyone else with an interest can find schools fitting a particular set of criterion relevant to them, for example, schools close to home, with times in a particular range, and of a specified academic profile.")),
+                                          br(),
+                                          h5(p("The project began as an attempt to combine my interest in swimming with a need to practice R, a programming language used primarily for analyzing and reporting data.  It has two components.  The first is this app, which queries a dataset to return information in the form of plots, data tables etc.  The second is the dataset itself, which I assembled by tying together information from the sources below.")),
+                                          br(),
+                                          h5(p("I hope you find it interesting and/or useful.  Any comments or questions are welcome at gpilgrim2607@gmail.com"),
+                                             p("The source code for this Shiny app is available ", a("on github", href = "https://github.com/gpilgrim2670/SwimMap"), "."))
+
+                                          #hr(),
+
+                                   ),
+                                 )
+                        )
+             ),
+             # end  navbarMenu
+             tabPanel("Prepare Data", fluid = TRUE, icon = icon("globe-americas"),
                       #tags$style(button_color_css),
                       # Sidebar layout with a input and output definitions
                       sidebarLayout(
@@ -72,6 +113,7 @@ ui <- fluidPage(
 
                       )
             ),
+
        )
   )
 
